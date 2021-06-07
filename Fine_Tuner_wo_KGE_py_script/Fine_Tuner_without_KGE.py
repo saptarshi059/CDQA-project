Fixed EM calculation & completed custom_question_gen

Fine_Tuner_wo_KGE_py_script/Fine_Tuner_without_KGE.py
@@ -122,7 +122,7 @@ def compute_EM(df):
         a_gold = df['true_answer'][i]
         a_pred = df['predicted_answer'][i]
         EM.append(int(normalize_answer(a_gold) == normalize_answer(a_pred)))
-    return np.sum(EM)
+    return np.mean(EM)
 
 def compute_f1_main(df):
     F1 = []

covid_qa_baseline.py
@@ -163,4 +163,4 @@ if __name__ == '__main__':
         EM.append(compute_EM(a_gold, a_pred))
 
     print(f"Avg. F1: {np.mean(F1)}")
-    print(f"Total EM: {np.sum(EM)}")
+    print(f"Total EM: {np.mean(EM)}")