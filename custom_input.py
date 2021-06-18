aa588434e9c631e87bf85339624dc61c469fd353
c-heat16 <czh5372@psu.edu>
2021-06-18 10:52:01 -0400

    Padding & attn mask

--- custom_input.py
+++ custom_input.py
@@ -1,5 +1,5 @@
-#Extracting embeddings for non-domain terms. I'm simply using BERT's tokenizer for the nDT's.
-#Creating question representations in this block.
+# Extracting embeddings for non-domain terms. I'm simply using BERT's tokenizer for the nDT's.
+# Creating question representations in this block.
 from transformers import AutoTokenizer, AutoModel
 import torch
 import re
@@ -7,7 +7,7 @@ import os
 import pandas as pd
 import re
 
-#DTE_Model_Lookup_Table = pd.read_pickle(os.path.join(os.path.abspath('UMLS_KG'), 'embeddings/distmult/DTE_to_BERT.pkl'))
+# DTE_Model_Lookup_Table = pd.read_pickle(os.path.join(os.path.abspath('UMLS_KG'), 'embeddings/distmult/DTE_to_BERT.pkl'))
 
 DTE_Model_Lookup_Table = pd.read_pickle('DTE_to_RoBERTa.pkl')
 Metamap_Tokenizations = pd.read_pickle('Metamap_Tokenizations.pkl')
@@ -24,6 +24,7 @@ SEP_embedding = model_embeddings(torch.LongTensor([tokenizer.sep_token_id]))
 
 all_entities = DTE_Model_Lookup_Table['Term'].to_list()
 
+
 def custom_input_rep(ques, context):
     ques = re.sub(' +', ' ', ques).strip()
 
@@ -33,15 +34,15 @@ def custom_input_rep(ques, context):
     tup = Metamap_Tokenizations.query("Question==@ques")
     print('ques: \"{}\"'.format(ques))
     print('context: \"{}\"'.format(context))
-    print('tup: {}'.format(tup))    # debugging
+    print('tup: {}'.format(tup))  # debugging
     print('tup columns: {}'.format(tup.columns))
     print('tup[\'Tokenization\']: {}'.format(tup['Tokenization']))
-    
+
     metamap_tokenized_question = tup['Tokenization'].values[0]
 
-    #Removing punctuations/spaces from domain-terms for easy comparison
+    # Removing punctuations/spaces from domain-terms for easy comparison
     mappings = tup['Mappings'].values[0]
-    for i,x in enumerate(mappings):
+    for i, x in enumerate(mappings):
         mappings[i][0] = clean_term(x[0])
 
     domain_terms = [x[0] for x in mappings]
@@ -59,39 +60,56 @@ def custom_input_rep(ques, context):
         then use its BERT embeddings.
         '''
 
-        if filtered_word in domain_terms: #Use DTE_BERT_Matrix
+        if filtered_word in domain_terms:  # Use DTE_BERT_Matrix
             mapped_concept = mappings[domain_terms.index(filtered_word)][1]
-            if mapped_concept in all_entities: 
+            if mapped_concept in all_entities:
                 question_embeddings.append(DTE_Model_Lookup_Table.query("Term==@mapped_concept")['Embedding'].values[0])
-            
-        #The mapped_concept doesn't have an expansion in the KG or the term isn't a DT. Thus, its BERT embeddings are used.
+
+        # The mapped_concept doesn't have an expansion in the KG or the term isn't a DT. Thus, its BERT embeddings are used.
         else:
-            subword_indices = tokenizer(word)['input_ids'][1:-1] #Take all tokens between [CLS] & [SEP]
+            subword_indices = tokenizer(word)['input_ids'][1:-1]  # Take all tokens between [CLS] & [SEP]
             for index in subword_indices:
                 question_embeddings.append(model_embeddings(torch.LongTensor([index])))
-    
-    #Since our total i/p's can only be 512 tokens long, the context has to be adjusted accordingly.
+
+    # Since our total i/p's can only be 512 tokens long, the context has to be adjusted accordingly.
     len_custom_question = len(question_embeddings)
     max_length = 512
-    limit_for_context = max_length - (len_custom_question + 2) #2 to account for [CLS] & [SEP]
-    
+    limit_for_context = max_length - (len_custom_question + 2)  # 2 to account for [CLS] & [SEP]
+
     context_embeddings = []
-    
-    #Taking all tokens b/w 1 & limit_for_context
-    reduced_context_indices = tokenizer(context, truncation=True)['input_ids'][1:limit_for_context+1]
-    
+
+    # Taking all tokens b/w 1 & limit_for_context
+    reduced_context_indices = tokenizer(context, truncation=True)['input_ids'][1:limit_for_context + 1]
+
     for index in reduced_context_indices:
         context_embeddings.append(model_embeddings(torch.LongTensor([index])))
-        
-    #In this way, I don't have to add the CLS & SEP embeddings during fine-tuning.
-    final_representation = torch.unsqueeze(torch.cat((CLS_embedding,\
-                                                      torch.cat([*question_embeddings]),\
-                                                      torch.cat([*context_embeddings]),\
-                                                      SEP_embedding)), dim=1)
 
-    input('final_representation: {}'.format(final_representation.shape))
+    # In this way, I don't have to add the CLS & SEP embeddings during fine-tuning.
+    # final_representation = torch.unsqueeze(torch.cat((CLS_embedding,\
+    #                                                   torch.cat([*question_embeddings]),\
+    #                                                   torch.cat([*context_embeddings]),\
+    #                                                   SEP_embedding)), dim=1)
 
-    #This difference will be used to adjust the start/end indices of the answers in context.
+    final_representation = torch.cat((CLS_embedding,
+                                      torch.cat([*question_embeddings]),
+                                      torch.cat([*context_embeddings]),
+                                      SEP_embedding))
+
+    n_pad = max_length - final_representation.shape[0]
+    attn_mask = torch.zeros((max_length, max_length))
+    for mask_idx in range(n_pad):
+        attn_mask[-(mask_idx + 1), :] = 0
+        attn_mask[:, -(mask_idx + 1)] = 0
+
+    if n_pad > 0:
+        model_dim = final_representation.shape[-1]
+        new_padding = torch.zeros((n_pad, model_dim))
+        final_representation = torch.cat([final_representation, new_padding], dim=0)
+
+    print('!! attn_mask: {} !!'.format(attn_mask.shape))
+    print('!! final_representation: {} !!'.format(final_representation.shape))
+
+    # This difference will be used to adjust the start/end indices of the answers in context.
     token_diff = len(tokenizer(ques)['input_ids']) - len(question_embeddings)
-       
-    return final_representation, token_diff
+
+    return final_representation, token_diff, attn_mask
--- run_modeling.py
+++ run_modeling.py
@@ -353,17 +353,19 @@ if __name__ == '__main__':
                 end_positions = batch['end_positions'].to(device)
 
                 if USE_KGE:
-                    input_embds, offsets = [], []
+                    input_embds, offsets, attn_masks = [], [], []
                     for q_text, c_text in zip(question_texts, context_texts):
                         #re.sub(' +', ' ', q_text) this was returning a string, I guess if you assigned it to q_text, it would have worked. 
-                        this_input_embds, this_n_token_adj = custom_input_rep(q_text, c_text)
+                        this_input_embds, this_n_token_adj, this_attention_mask = custom_input_rep(q_text, c_text)
                         # print('this_n_token_adj: {}'.format(this_n_token_adj))
                         this_n_token_adj = torch.tensor([[this_n_token_adj]])
                         input_embds.append(this_input_embds.unsqueeze(0))
+                        attn_masks.append(this_attention_mask.unsqueeze(0))
                         offsets.append(this_n_token_adj)
 
                     input_embds = torch.cat(input_embds, dim=0).to(device)
                     offsets = torch.cat(offsets, dim=0).to(device)
+                    attention_mask = torch.cat(attn_masks, dim=0).to(device)
 
                     start_positions = start_positions - offsets
                     end_positions = end_positions - offsets
@@ -373,8 +375,8 @@ if __name__ == '__main__':
                     input_embds = model_embds(input_ids)
 
                 print('*' * 50)
-                print('input_embds: {}'.format(input_embds.shape))
                 print('attention_mask: {}'.format(attention_mask.shape))
+                print('input_embds: {}'.format(input_embds.shape))
                 print('start_positions: {}'.format(start_positions.shape))
                 print('end_positions: {}'.format(end_positions.shape))
                 print('*' * 50)
