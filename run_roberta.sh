3e4cfae94462f0f651c20b435a72575cc09bcdc4
connor heaton <czh5372@psu.edu>
2021-06-30 17:05:38 -0400

    Less printing

--- run_roberta.sh
+++ run_roberta.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 export CUDA_VISIBLE_DEVICES=0
-python run_modeling.py --batch_size 32 \
+nohup python run_modeling.py --batch_size 32 \
 		       --model_name "navteca/roberta-base-squad2" \
 		       --lr 1e-4 \
 		       --n_epochs 2 \
-		       --use_kge T
+		       --use_kge T > "run_roberta.out" &
