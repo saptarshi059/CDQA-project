4c11219d3fc69c84e555c3afefc43339267415c5
connor heaton <czh5372@psu.edu>
2021-07-01 12:45:30 -0400

    New parms

--- run_roberta_kge.sh
+++ run_roberta_kge.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 python run_modeling.py --batch_size 32 \
 		       --model_name "navteca/roberta-base-squad2" \
-		       --lr 1e-4 \
+		       --lr 3e-5 \
 		       --n_epochs 2 \
+		       --n_stride 192 \
 		       --use_kge T
--- run_roberta_vanilla.sh
+++ run_roberta_vanilla.sh
@@ -3,6 +3,7 @@
 export CUDA_VISIBLE_DEVICES=0
 python run_modeling.py --batch_size 32 \
 		       --model_name "navteca/roberta-base-squad2" \
-		       --lr 1e-4 \
+		       --lr 3e-5 \
 		       --n_epochs 2 \
+		       --n_stride 192 \
 		       --use_kge F
