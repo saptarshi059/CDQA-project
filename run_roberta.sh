0dce25c516f7c278ec60ae69fae0cb720cb3cd19
connor heaton <czh5372@psu.edu>
2021-06-30 20:33:48 -0400

    torch.no_grad() on custom_input

--- run_roberta.sh
+++ run_roberta.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export CUDA_VISIBLE_DEVICES=0
-nohup python run_modeling.py --batch_size 32 \
+python run_modeling.py --batch_size 32 \
 		       --model_name "navteca/roberta-base-squad2" \
 		       --lr 1e-4 \
 		       --n_epochs 2 \
-		       --use_kge T > "run_roberta.out" &
+		       --use_kge T
