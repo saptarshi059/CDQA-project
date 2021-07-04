5ebee214f8aa81bee756c8d356133aa45efee9df
connor heaton <czh5372@psu.edu>
2021-07-04 10:45:15 -0400

    Full send

--- run_roberta_vanilla.sh
+++ run_roberta_vanilla.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0,1,2
 python run_modeling.py --batch_size 32 \
 		       --model_name "navteca/roberta-base-squad2" \
 		       --lr 3e-5 \
 		       --n_epochs 2 \
 		       --n_stride 192 \
 		       --use_kge F \
-		       --gpus 0 1 \
+		       --gpus 0 1 2 \
 		       --port 42069
