8c0bedb2dabfdd28720088cc2b3c843a99354908
connor heaton <czh5372@psu.edu>
2021-07-05 08:51:30 -0400

    Batch size

--- run_roberta_vanilla.sh
+++ run_roberta_vanilla.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=0,1,2
-python run_modeling.py --batch_size 32 \
+export CUDA_VISIBLE_DEVICES=0,1
+python run_modeling.py --batch_size 40 \
 		       --model_name "navteca/roberta-base-squad2" \
 		       --lr 3e-5 \
 		       --n_epochs 2 \
 		       --max_len 384 \
 		       --n_stride 192 \
 		       --use_kge F \
-		       --gpus 0 1 2 \
+		       --gpus 0 1 \
 		       --port 42069
