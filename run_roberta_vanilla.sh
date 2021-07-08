a58e7b5967a3770f50598e7be5e89b64a3642b1a
connor heaton <czh5372@psu.edu>
2021-07-08 09:55:46 -0400

    Warmup proportion fixes

--- run_roberta_vanilla.sh
+++ run_roberta_vanilla.sh
@@ -4,9 +4,9 @@ export CUDA_VISIBLE_DEVICES=0,1,2
 python run_modeling.py --batch_size 24 \
 		       --model_name "deepset/roberta-base-squad2" \
 		       --lr 3e-5 \
-		       --n_epochs 3 \
+		       --n_epochs 2 \
 		       --max_len 384 \
-		       --n_stride 128 \
+		       --n_stride 196 \
 		       --warmup_proportion 0.1 \
 		       --use_kge F \
 		       --gpus 0 1 2 \
