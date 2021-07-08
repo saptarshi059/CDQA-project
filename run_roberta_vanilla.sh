083fbe55f724f2b3b9c21003176960a739e6a73d
connor heaton <czh5372@psu.edu>
2021-07-08 16:46:16 -0400

    Parms

--- run_roberta_vanilla.sh
+++ run_roberta_vanilla.sh
@@ -5,8 +5,8 @@ python run_modeling.py --batch_size 24 \
 		       --model_name "navteca/roberta-base-squad2" \
 		       --lr 3e-5 \
 		       --n_epochs 2 \
-		       --max_len 512 \
-		       --n_stride 196 \
+		       --max_len 384 \
+		       --n_stride 128 \
 		       --warmup_proportion 0.1 \
 		       --use_kge F \
 		       --gpus 0 1 2 \
