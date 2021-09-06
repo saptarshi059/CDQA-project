dbc59a258082ddffb1b58605bf96a822fd47f44d
saptarshi059 <ssengupta8@gmail.com>
2021-09-06 12:07:01 -0400

    CMDLine options for Mik4KG.py

--- Homogenization_Programs/Mikolov++/Mik4KG.py
+++ Homogenization_Programs/Mikolov++/Mik4KG.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-#python Mik4KG.py --UMLS_Path ../../../Train_KGE/UMLS_KG_MT+SN --BERT_Variant navteca/roberta-base-squad2
+#python Mik4KG.py --UMLS_Path ../../../Train_KGE/UMLS_KG_MT+SN --BERT_Variant navteca/roberta-base-squad2 --THROUGH True --TRIPLES True --HS False --PO True
 
 import pickle5 as pickle
 import pandas as pd
@@ -17,7 +17,16 @@ logging.set_verbosity(50)
 parser = argparse.ArgumentParser()
 parser.add_argument('--UMLS_Path', type=str)
 parser.add_argument('--BERT_Variant', type=str)
+parser.add_argument('--THROUGH', type=bool)
+parser.add_argument('--TRIPLES', type=bool)
+parser.add_argument('--HS', type=bool)
+parser.add_argument('--PO', type=bool)
 args = parser.parse_args()
+
+if args.TRIPLES == True:
+    MRREL_rel2desc = pd.read_pickle('MRREL_rel2desc.pkl')
+    SEM_NW_rel2desc = pd.read_pickle('SEM_NW_rel2desc.pkl')
+    total_rel2desc = pd.concat([MRREL_rel2desc, SEM_NW_rel2desc], ignore_index=True).drop_duplicates()
 
 #Mapping b/w entity and corresponding ID
 with open(os.path.join(os.path.abspath(args.UMLS_Path), 'entity2idx.pkl'), 'rb') as f:
@@ -44,15 +53,52 @@ print('Necessary Files Loaded...')
 
 src = []
 tgt = []
-for ent_name, ent_index in tqdm(entity2id.items()):
-    entity_tokens = tokenizer(ent_name, return_tensors='pt')['input_ids'][0]
-    sw_embds = []
-    for index in range(1, len(entity_tokens)-1):
-        sw_embds.append(model_embeddings(entity_tokens[index]))
-    src.append(ent_embeddings.iloc[ent_index].to_numpy())
-    tgt.append(torch.mean(torch.vstack(sw_embds), dim=0).detach().numpy())
 
-weight_matrix = np.linalg.lstsq(np.vstack(src),np.vstack(tgt), rcond=None)[0]
+if args.THROUGH == False:
+    for ent_name, ent_index in tqdm(entity2id.items()):
+        entity_tokens = tokenizer(ent_name, return_tensors='pt')['input_ids'][0]
+        sw_embds = []
+        for index in range(1, len(entity_tokens)-1):
+            sw_embds.append(model_embeddings(entity_tokens[index]))
+        src.append(ent_embeddings.iloc[ent_index].to_numpy())
+        tgt.append(torch.mean(torch.vstack(sw_embds), dim=0).detach().numpy())
+
+    weight_matrix = np.linalg.lstsq(np.vstack(src),np.vstack(tgt), rcond=None)[0]
+else:
+    number_of_layers = 4
+    with torch.no_grad():
+        if args.TRIPLES == True:
+            for triple in tqdm(KGT.itertuples()):
+                natural_text = triple.E1 + ' ' + total_rel2desc.query('REL==@triple.Rel').Description.values[0] + ' ' + triple.E2
+                inputs = tokenizer(natural_text, return_tensors='pt')
+                inputs.to(device)
+                output = model(**inputs)
+                if args.PO == True:
+                    src.append(ent_embeddings.iloc[entity2id[triple.E1]].to_numpy())
+                    tgt.append(output['pooler_output'].detach().cpu().numpy().reshape(1,-1))
+                else:#[CLS] output
+                    src.append(ent_embeddings.iloc[entity2id[triple.E1]].to_numpy())
+                    tgt.append(output['last_hidden_state'][0][0].detach().cpu().numpy().reshape(1,-1))
+        else:
+            for ent_name, ent_index in tqdm(entity2id.items()):
+                inputs = tokenizer(ent_name, return_tensors='pt')
+                inputs.to(device)
+                output = model(**inputs, output_hidden_states=True)
+                if args.HS == True:
+                    entity_tokens = output['hidden_states'][0][0].shape[0] - 2
+                    vecs = []
+                    for layer_idx in range(number_of_layers):
+                        vecs.append([output['hidden_states'][layer_idx][0][i] for i in range(1,  entity_tokens + 1)])
+                    c = torch.vstack([torch.hstack(vecs[x]).reshape(1, entity_tokens, 768) for x in range(number_of_layers)])
+                    src.append(ent_embeddings.iloc[ent_index].to_numpy())
+                    tgt.append(torch.mean(torch.mean(c, dim=0), dim=0).detach().cpu().numpy().reshape(1,-1))
+                else:          
+                    if args.PO == True:
+                        src.append(ent_embeddings.iloc[ent_index].to_numpy())
+                        tgt.append(output['pooler_output'].detach().cpu().numpy().reshape(1,-1))
+                    else: #[CLS] output
+                        src.append(ent_embeddings.iloc[ent_index].to_numpy())
+                        tgt.append(output['last_hidden_state'][0][0].detach().cpu().numpy().reshape(1,-1))
 
 homogenized_embeddings = {}
 for entity_name, index in tqdm(entity2id.items()):
