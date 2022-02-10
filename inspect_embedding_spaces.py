bc83deaf7b0085b9a5c961e5fda5fbd72357ca39
czh <c.heat16@yahoo.com>
2022-02-10 08:22:41 -0500

    Unity

--- inspect_embedding_spaces.py
+++ inspect_embedding_spaces.py
@@ -55,8 +55,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--dte_lookup_table_fp',
-                        default='Mikolov++_to_phiyodr_bert-base-finetuned-squad2.pkl'
-                        # default='DTE_to_phiyodr_bert-base-finetuned-squad2.pkl',
+                        default='NN-DTE-to-phiyodr-bert-base-finetuned-squad2.pkl'
+                        # default='Mikolov_to_phiyodr_bert-base-finetuned-squad2.pkl',
                         # default='DTE_to_ktrapeznikov_biobert_v1.1_pubmed_squad_v2.pkl',
                         # default='DTE_to_ktrapeznikov_scibert_scivocab_uncased_squad_v2.pkl'
                         )
@@ -69,7 +69,7 @@ if __name__ == '__main__':
                         # default='ktrapeznikov/scibert_scivocab_uncased_squad_v2',
                         help='Type of model to use from HuggingFace')
     parser.add_argument('--out', default='out/embedding_space_inspection', help='Directory to put output')
-    parser.add_argument('--tsne_perplexity', default=75, type=int)
+    parser.add_argument('--tsne_perplexity', default=25, type=int)
 
     args = parser.parse_args()
 
@@ -85,7 +85,8 @@ if __name__ == '__main__':
     plt_fp = os.path.join(outdir, 'embedding_space_viz_{}perp.png'.format(args.tsne_perplexity))
 
     DTE_Model_Lookup_Table = pickle.load(open(args.dte_lookup_table_fp, 'rb'))
-    dtes = DTE_Model_Lookup_Table['Embedding'].tolist()
+    print('DTE_Model_Lookup_Table.columns: {}'.format(DTE_Model_Lookup_Table.columns))
+    dtes = DTE_Model_Lookup_Table['UMLS_Embedding'].tolist()
     dtes = np.vstack(dtes)
     print('dtes.shape: {}'.format(dtes.shape))
     n_dte = dtes.shape[0]
@@ -106,28 +107,37 @@ if __name__ == '__main__':
     agg_tsne = do_tsne(agg_pca, perplexity=args.tsne_perplexity, n_iter=1000)
 
     initial_tsne = agg_tsne[:n_initial]
-    first_1000_tsne = initial_tsne[:1000]
+    first_1000_tsne = initial_tsne[:999]
     # initial_tsne = initial_tsne[1000:]
     dte_tsne = agg_tsne[-n_dte:]
     print('initial_tsne: {}'.format(initial_tsne.shape))
     print('first_1000_tsne: {}'.format(first_1000_tsne.shape))
     print('dte_tsne: {}'.format(dte_tsne.shape))
 
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    tokenizer = BertTokenizer.from_pretrained(args.model_name)
+    # first_1000_ids = [i for i in range(999)]
+    # first_1000_tokens = tokenizer.convert_ids_to_tokens(first_1000_ids)
+    # for i, t in enumerate(first_1000_tokens):
+    #     print('{}: {}'.format(i, t))
+    # input('okty')
     cls_id = tokenizer.convert_tokens_to_ids('[CLS]')
     pad_id = tokenizer.convert_tokens_to_ids('[PAD]')
     sep_id = tokenizer.convert_tokens_to_ids('[SEP]')
+    unk_id = tokenizer.convert_tokens_to_ids('[UNK]')
+    mask_id = tokenizer.convert_tokens_to_ids('[MASK]')
 
     print('Plotting...')
-    plt.scatter(initial_tsne[:, 0], initial_tsne[:, 1], c='green', label='OG')
+    plt.scatter(initial_tsne[:, 0], initial_tsne[:, 1], c='green', label='Contextual')
     plt.scatter(dte_tsne[:, 0], dte_tsne[:, 1], c='red', label='DTE')
-    plt.scatter(first_1000_tsne[:, 0], first_1000_tsne[:, 1], c='pink', label='1000')
+    plt.scatter(first_1000_tsne[:, 0], first_1000_tsne[:, 1], c='pink', label='[UNUSED]')
 
     plt.scatter(initial_tsne[cls_id, 0], initial_tsne[cls_id, 1], c='black', label='[CLS]')
     plt.scatter(initial_tsne[pad_id, 0], initial_tsne[pad_id, 1], c='orange', label='[PAD]')
     plt.scatter(initial_tsne[sep_id, 0], initial_tsne[sep_id, 1], c='blue', label='[SEP]')
+    plt.scatter(initial_tsne[unk_id, 0], initial_tsne[unk_id, 1], c='purple', label='[UNK]')
+    plt.scatter(initial_tsne[mask_id, 0], initial_tsne[mask_id, 1], c='yellow', label='[MASK]')
 
-    plt.scatter(initial_tsne[7592, 0], initial_tsne[7592, 1], c='brown', label='hello')
+    # plt.scatter(initial_tsne[7592, 0], initial_tsne[7592, 1], c='brown', label='hello')
 
     plt.legend(bbox_to_anchor=(1, 1))
     plt.title('{}\nEmbedding Space'.format(kge_name))
