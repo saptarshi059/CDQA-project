ce6916470080651440fe1cf61bcf7e183ab723f8
czh <c.heat16@yahoo.com>
2022-02-15 11:08:07 -0500

    DTE fix

--- runners.py
+++ runners.py
@@ -86,7 +86,7 @@ class PubmedQARunner(object):
 
             if args.random_kge:
                 print('Replacing DTEs with random tensors...')
-                dtes = [torch.rand(1, 768) for _ in self.dtes]
+                dtes = [torch.rand(1, 768) for _ in dtes]
 
             print('dtes[0]: {}'.format(dtes[0]))
             self.dtes = torch.cat(dtes, dim=0).to(self.device)
